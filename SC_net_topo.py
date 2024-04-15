07fbfd1c6fd3d50ff25f90b4c99cf00285c26954
Harshavardhan-BV <bharshavardh@iisc.ac.in>
2024-04-15 10:53:56 +0530

    Fixed impure_n results and added till 6 nodes

--- SC_net_topo.py
+++ SC_net_topo.py
@@ -28,9 +28,14 @@ for n in range(2,9):
         for density in range(2,7,2):
             ng.embedded_toggle(n, size, density, 100)
 #%%
-# Impure for Toggle 5 network
-n = 5
+# Impure for Toggle 2-5 network
+for n in range(2,6):
+    m_max = n*(n-1)
+    for m in range(m_max+1):
+        ng.impure_n(n,m, 100)
+# %%
+# Impure for Toggle 6 network, 50 each
+n = 6
 m_max = n*(n-1)
 for m in range(m_max+1):
-    ng.impure_n(n,m, 100)
-# %%
+    ng.impure_n(n,m, 50)
